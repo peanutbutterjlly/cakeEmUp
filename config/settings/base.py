0eda427f0b21fd5a7490017168bc1f28a749bab3
peanutbutterjlly <steverios_sr@yahoo.com>
2023-01-31 18:37:45 -0600

    updated settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -101,10 +101,6 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-STATICFILES_DIRS = [
-    BASE_DIR / "apps" / "theme" / "static",
-]
-
 STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
