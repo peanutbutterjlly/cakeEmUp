7ecdfc203b6355a24cf062252b3c7968ab3ec899
peanutbutterjlly <steverios_sr@yahoo.com>
2023-01-31 18:43:33 -0600

    removed unused staticfiles directory

--- staticfiles/js/scripts.js
+++
@@ -1,6 +0,0 @@
-const menuButton = document.getElementById('menu-btn');
-const menuItems = document.getElementById('menu-items');
-menuButton.addEventListener('click', () => {
-  menuItems.classList.toggle('hidden');
-  menuItems.classList.toggle('flex');
-});
