744a8929c4141eafcb358a6c61c2a7ba517bf12b
peanutbutterjlly <steverios_sr@yahoo.com>
2023-01-31 18:35:00 -0600

    moved static files

--- config/settings/base.py
+++ config/settings/base.py
@@ -102,11 +102,10 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
     BASE_DIR / "apps" / "theme" / "static",
 ]
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
